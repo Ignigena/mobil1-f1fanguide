3bec3a9bd1ee54228ccd03637ee5b78ca0b97983
Albert Martin <albert@ilovetheory.com>
2012-09-24 17:23:31 -0400

    Finishing off Technology slider

--- Mobil 1 Fan Guide/M1TechnologyViewController.h
+++ Mobil 1 Fan Guide/M1TechnologyViewController.h
@@ -10,9 +10,21 @@
 
 @interface M1TechnologyViewController : UIViewController <UIScrollViewDelegate>
 
+@property (nonatomic) CGPoint carsTextOrigin;
+@property (nonatomic) CGPoint driversTextOrigin;
 @property (nonatomic) CGPoint driversOrigin;
+@property (nonatomic) CGPoint fanFestTextOrigin;
+@property (nonatomic) CGPoint trackTextOrigin;
+@property (nonatomic) CGPoint trackOrigin;
+@property (nonatomic) CGPoint tourOrigin;
 
 @property (strong, nonatomic) IBOutlet UIScrollView *technologyScroller;
+@property (strong, nonatomic) IBOutlet UIButton *carsText;
+@property (strong, nonatomic) IBOutlet UIButton *driversText;
 @property (strong, nonatomic) IBOutlet UIButton *driversButton;
+@property (strong, nonatomic) IBOutlet UIButton *fanFestText;
+@property (strong, nonatomic) IBOutlet UIButton *trackText;
+@property (strong, nonatomic) IBOutlet UIButton *trackButton;
+@property (strong, nonatomic) IBOutlet UIButton *tourButton;
 
 @end
--- Mobil 1 Fan Guide/M1TechnologyViewController.m
+++ Mobil 1 Fan Guide/M1TechnologyViewController.m
@@ -28,8 +28,14 @@
 {
     [super viewDidLoad];
     
-    _technologyScroller.contentSize = CGSizeMake(self.technologyScroller.frame.size.width*2, self.technologyScroller.contentSize.height);
+    _technologyScroller.contentSize = CGSizeMake(self.technologyScroller.frame.size.width*3, self.technologyScroller.contentSize.height);
+    _carsTextOrigin = self.carsText.frame.origin;
     _driversOrigin = self.driversButton.frame.origin;
+    _driversTextOrigin = self.driversText.frame.origin;
+    _fanFestTextOrigin = self.fanFestText.frame.origin;
+    _trackTextOrigin = self.trackText.frame.origin;
+    _trackOrigin = self.trackButton.frame.origin;
+    _tourOrigin = self.tourButton.frame.origin;
 	
     CAGradientLayer *gradient = [CAGradientLayer layer];
     gradient.frame = self.view.bounds;
@@ -41,7 +47,13 @@
 {
     float offset = 0-scrollView.contentOffset.x;
     
-    self.driversButton.frame = CGRectMake(self.driversOrigin.x+(self.driversOrigin.x*(offset/400)), self.driversButton.frame.origin.y, self.driversButton.frame.size.width, self.driversButton.frame.size.height);
+    self.carsText.frame = CGRectMake(self.carsTextOrigin.x+(self.carsTextOrigin.x*(0-offset/50)), self.carsText.frame.origin.y, self.carsText.frame.size.width, self.carsText.frame.size.height);
+    self.driversButton.frame = CGRectMake(self.driversOrigin.x+(self.driversOrigin.x*(offset/450)), self.driversButton.frame.origin.y, self.driversButton.frame.size.width, self.driversButton.frame.size.height);
+    self.driversText.frame = CGRectMake(self.driversTextOrigin.x+(self.driversTextOrigin.x*(offset/500)), self.driversText.frame.origin.y, self.driversText.frame.size.width, self.driversText.frame.size.height);
+    self.fanFestText.frame = CGRectMake(self.fanFestTextOrigin.x+(self.fanFestTextOrigin.x*(offset/2000)), self.fanFestText.frame.origin.y, self.fanFestText.frame.size.width, self.fanFestText.frame.size.height);
+    self.trackButton.frame = CGRectMake(self.trackOrigin.x+(self.trackOrigin.x*(offset/3000)), self.trackButton.frame.origin.y, self.trackButton.frame.size.width, self.trackButton.frame.size.height);
+    self.trackText.frame = CGRectMake(self.trackTextOrigin.x+(self.trackTextOrigin.x*(0-offset/10000)), self.trackText.frame.origin.y, self.trackText.frame.size.width, self.trackText.frame.size.height);
+    self.tourButton.frame = CGRectMake(self.tourOrigin.x+(self.tourOrigin.x*(offset/9000)), self.tourButton.frame.origin.y, self.tourButton.frame.size.width, self.tourButton.frame.size.height);
 }
 
 - (void)didReceiveMemoryWarning
