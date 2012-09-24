600eca490539995277f32bf14b12b4b2ee65e178
Albert Martin <albert@ilovetheory.com>
2012-09-24 14:15:32 -0400

    Remove some debug code

--- Mobil 1 Fan Guide/M1NavigationBar.m
+++ Mobil 1 Fan Guide/M1NavigationBar.m
@@ -22,16 +22,6 @@
 - (void)awakeFromNib
 {
     [self setBackgroundImage:[UIImage imageNamed:@"NavBar"] forBarMetrics:UIBarMetricsDefault];
-    NSLog(@"go go");
 }
-
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
 
 @end
