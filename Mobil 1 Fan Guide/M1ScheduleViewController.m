ce15437537acfeec428af06b93e4fa7f8aaeb522
Albert Martin <albert@ilovetheory.com>
2012-10-08 10:35:26 -0400

    Import MKNetworkKit header file

--- Mobil 1 Fan Guide/M1ScheduleViewController.m
+++ Mobil 1 Fan Guide/M1ScheduleViewController.m
@@ -14,6 +14,7 @@
 #import "M1ScheduleViewController.h"
 #import "M1TechnologyViewController.h"
 #import "NSString+RelativeDate.h"
+#import "MKNetworkKit.h"
 
 @interface M1ScheduleViewController ()
 
