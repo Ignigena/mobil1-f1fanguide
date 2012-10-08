235e669bbd0d25abc7af67add90ab04a1ca373d1
Albert Martin <albert@ilovetheory.com>
2012-10-08 10:33:46 -0400

    Fix bug with relative date display

--- Mobil 1 Fan Guide/NSString+RelativeDate.m
+++ Mobil 1 Fan Guide/NSString+RelativeDate.m
@@ -20,23 +20,25 @@
 	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
     
     if(dayDiff >= 365)
-        [dateFormatter setDateFormat:@"MMM d, YYYY"];
-    else if(dayDiff >= 31)
-        [dateFormatter setDateFormat:@"MMM d, h:mma"];
+        [dateFormatter setDateFormat:@"MMM d',' YYYY"];
+    else if(dayDiff >= 2)
+        [dateFormatter setDateFormat:@"MMM d',' h:mma"];
     else if(dayDiff == 1)
-		[dateFormatter setDateFormat:@"Tomorrow, h:mma"];
+		[dateFormatter setDateFormat:@"'Tomorrow,' h:mma"];
 	else if(dayDiff == 0)
-		[dateFormatter setDateFormat:@"Today, h:mma"];
+		[dateFormatter setDateFormat:@"'Today,' h:mma"];
 	else if(dayDiff == -1)
-		[dateFormatter setDateFormat:@"Yesterday"];
+		[dateFormatter setDateFormat:@"'Yesterday'"];
 	else if(dayDiff == -2)
-		[dateFormatter setDateFormat:@"Two days ago"];
+		[dateFormatter setDateFormat:@"'Two days ago'"];
 	else if(dayDiff > -14 && dayDiff <= -7)
-		[dateFormatter setDateFormat:@"Last week"];
-	else if(dayDiff >= -60 && dayDiff <= -30)
-		[dateFormatter setDateFormat:@"Last month"];
-	else if(dayDiff >= -90)
-		[dateFormatter setDateFormat:@"MMM d, YYYY"];
+		[dateFormatter setDateFormat:@"'Last week'"];
+    else if(dayDiff > -30 && dayDiff < -14)
+		[dateFormatter setDateFormat:@"'Two weeks ago'"];
+	else if(dayDiff > -60 && dayDiff <= -30)
+		[dateFormatter setDateFormat:@"'Last month'"];
+	else if(dayDiff <= -60)
+		[dateFormatter setDateFormat:@"MMM d',' YYYY"];
     
 	return [dateFormatter stringFromDate:date];
 }
